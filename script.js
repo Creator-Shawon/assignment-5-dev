aaca41391e14fd8d3acb41167251c344e6f7ad3a
creator-shawon <sha69won@gmail.com>
2025-03-19 20:59:19 +0600

    Final

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const allBtns = document.querySelectorAll('.completed-btn');
+const taskBtnsArray = Array.from(allBtns);
 
-for(const btn of allBtns){
-    btn.addEventListener('click', function(){
+
+for (const btn of allBtns) {
+    btn.addEventListener('click', function () {
 
         alert("You've successfully completed the task.")
         const navNumber = document.getElementById('nav-number');
@@ -15,7 +17,7 @@ for(const btn of allBtns){
         const convertedTaskValue = parseInt(taskValue);
         const taskReturn = convertedTaskValue - 1;
         taskNumber.innerText = taskReturn;
-        if(taskReturn <1){
+        if (taskReturn < 1) {
             alert("Congrates!! You've successfully completed all the task.")
         }
 
@@ -23,6 +25,37 @@ for(const btn of allBtns){
         btn.classList.remove('bg-purple-600')
         btn.classList.add('bg-gray-200');
 
-        
+
+        // Activity add to history log.
+        const selectedTitle = `title-${taskBtnsArray.indexOf(btn) + 1}`;
+        const title = document.getElementById(selectedTitle).innerText;
+        const historyContainer = document.getElementById('activity-log-container');
+
+        const getTime = currentTime(new Date());
+
+        const newHistory = document.createElement('div');
+        newHistory.innerHTML = `
+             <p class="bg-custom1 p-3 mb-4 rounded-md shadow-md">
+             You have completed the task <span class="font-semibold text-blue-500">${title}</span> at ${getTime}
+             </p>
+         `;
+
+        historyContainer.appendChild(newHistory)
+
+
     })
-}
+};
+
+document.getElementById('clearHistory').addEventListener('click', function () {
+    document.getElementById('activity-log-container').innerHTML = '';
+});
+
+
+document.getElementById('discover').addEventListener('click', function () {
+    window.location.href = 'blog.html'
+});
+
+
+document.getElementById('backtoDashboard').addEventListener('click', function () {
+    window.location.href = 'index.html'
+});
---
+++ utilities.js
@@ -0,0 +1,39 @@
+//Set current time
+function currentTime(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${hours}:${minutes}:${seconds} ${ampm}`
+}
+
+// Set Current Date
+
+function setCurrentDateByID(id) {
+    const today = new Date();
+
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const dayName = days[today.getDay()];
+    const date = String(today.getDate()).padStart(2, '0');
+    const monthName = months[today.getMonth()];
+    const year = today.getFullYear();
+
+    const currentDate = `${dayName}, ${date} ${monthName} ${year}`;
+
+    const expectedDate = document.createElement('div')
+
+    expectedDate.innerHTML = `
+        div
+    `
+
+    document.getElementById(id).innerText = currentDate;
+
+
+}
+
+// St Current Dt in Display by calling id name
+setCurrentDateByID('currentDate');
